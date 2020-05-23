ef348aaee9c914c5e71850b36f4dcca06a6765f2
Rezzubs <marten.roots@gmail.com>
2020-05-23 14:44:02 +0300

    added some fancy animations

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
-const coinflip = document.getElementById('coinflip')
-let rotateCount = 0
+const buttons = document.querySelectorAll(".button");
 
-function changeContent() {
-    let result = Math.round(Math.random())
-    
+const coinflip = document.querySelector(".middle");
 
-    if (result == coinflip.innerHTML) {
-        rotateCount += 20
-        coinflip.style.filter = "hue-rotate(" + rotateCount.toString() +  "deg)";
-    } else {
-        rotateCount = 0
-        coinflip.style.filter = "hue-rotate(0deg)"
-    }
-    
-    console.log(rotateCount);
-    
-    coinflip.innerHTML = result
-};
+const leftContent = document.querySelector('.leftContent')
+const rightContent = document.querySelector('.rightContent')
+const middleContent = document.querySelector('.middleContent')
+const content = document.querySelectorAll(".content");
 
-coinflip.onclick = changeContent
+const backButton = document.querySelectorAll(".backButton")
+
+
+let playAnimation = 1
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        // checks if the buttons have already been pressed
+        for (i = 0; i < buttons.length; i++) { 
+            
+            console.log(i + ' ' + buttons[i]);
+            if (buttons[i].classList.contains('toggleMargin') || buttons[i].classList.contains('extended')) {
+                playAnimation = 0
+                break
+            } else {
+                playAnimation = 1
+            }
+            
+        }
+        
+        //main code
+        if (playAnimation) {
+            coinflip.classList.add('toggleMargin');
+
+            if (button.classList.contains('left')) {
+                leftContent.classList.remove('invisible');
+                leftContent.classList.add('extended');
+                rightContent.classList.add('invisible')
+            } 
+            else if (button.classList.contains('middle')) {
+                middleContent.classList.remove('invisible');
+                middleContent.classList.add('extended');
+                rightContent.classList.add('invisible');
+                leftContent.classList.add('invisible');
+            } 
+            else if (button.classList.contains('right')) {
+                rightContent.classList.remove('invisible');
+                rightContent.classList.add('extended')
+                leftContent.classList.add('invisible')
+            }
+        }
+    })
+})
+
+backButton.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(content);
+    
+        coinflip.classList.remove('toggleMargin');
+        
+        content.forEach(cnt => {
+            console.log(cnt);
+            
+            cnt.classList.remove('extended')
+        })
+    })
+})
